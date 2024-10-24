39ee63011d2e6511db94c1c7fe0f615f2cc9bf03
jasmintawfik <jasmintawfik7@gmail.com>
2024-10-24 20:26:33 +0200

    Add files via upload

---
+++ Task 6 ud 1/src/Flow.java
@@ -0,0 +1,47 @@
+public class Flow {
+
+    public void name1() {
+        System.out.println("J");
+    }
+
+    public void name2() {
+        System.out.println("A");
+
+    }
+
+    public void name3() {
+        System.out.println("V");
+
+    }
+
+    public void name4() {
+        System.out.println("A");
+    }
+
+
+    public void name5 (){
+        System.out.println("E");
+
+    }
+
+    public void name6 (){
+        System.out.println("R");
+    }
+    public void name7 (){
+        System.out.println("S");
+    }
+    public void name8 (){
+        System.out.println("J");
+    }
+    public void name9 (){
+        System.out.println("O");
+
+    }
+    public void name10 (){
+        System.out.println("V");
+    }
+    public void name11 (){
+        System.out.println("T");
+    }
+}
+
---
+++ Task 6 ud 1/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Flow flow = new Flow();
+        flow.name1();
+        flow.name2();
+        flow.name3();
+        flow.name4();
+        flow.name5();
+        flow.name6();
+        flow.name7();
+        flow.name8();
+        flow.name9();
+        flow.name10();
+        flow.name11();
+
+
+    }
+}
